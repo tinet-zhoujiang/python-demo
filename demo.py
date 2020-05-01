92b85a13c656f677937427c33b21402cfd10fd9b
zhoujiang <969385836@qq.com>
2020-05-01 22:00:36 +0800

    add demo.py

---
+++ demo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+print("practice if else condition expression")
+if True:
+    print("if condition is true")
+else:
+    print("if condition is fasle")
+if False:
+    print("if condition is true")
+else:
+    print("if condition is fasle")
