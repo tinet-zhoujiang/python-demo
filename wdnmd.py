637719b483f65170a3df449bb216afeebbe3c28c
aoliaodemao <1064272049@qq.com>
2020-05-01 23:53:47 +0800

    for

---
+++ wdnmd.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+a =7
+if a<9:
+    print("hello world")
+elif a<10:
+    print("wdnmd")
+else:
+    a +=1
+print(a)
+
+
---
+++ while.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# /*
+#  a=1 
+# while a<10:
+#     print ("hello") , a
+#     a +=1
+# */
+# for a in 'wdnmd':
+#     print ('字母',a)
+
+# for x in ['zhangsna','lisi','wangwu']:
+#     if x == 'lisi':
+#         print('x')
+#         break
+#     print(x)
+# for x in ['zhangsna','lisi','wangwu']:
+#     if x == 'lisi':
+#         #print ('x')
+#         continue
+#     print(x)
+# for x in ['zhangsna','lisi','wangwu']:
+    
+#     pass
+#     print('a')
+    
+        
+    
