04058b71b0b555afc4896935451f33a9db2751d7
zhoujiang <969385836@qq.com>
2020-05-02 00:05:46 +0800

    study python day 01

--- demo.py
+++ demo.py
@@ -1,12 +1,47 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
 
-print("practice if else condition expression")
-if True:
-    print("if condition is true")
-else:
-    print("if condition is fasle")
-if False:
-    print("if condition is true")
-else:
-    print("if condition is fasle")
+#if else condition expression
+# if True:
+#     print("if condition is true")
+# else:
+#     print("if condition is fasle")
+# if False:
+#     print("if condition is true")
+# else:
+#     print("if condition is fasle")
+
+# num = 1
+# if num > 0:
+#     print("a>0")
+# elif num > 1:
+#     print("a>1")
+
+
+# while
+# num = 2
+# while num <= 10:
+#     print("num:", num)
+#     num += 1
+
+
+## for
+# for aaa in "asfasfgasga":
+#     print(aaa)
+
+## for list
+# for aaaa in ["aaa", "bbb", "ccc"]:
+#     if aaaa == "aaa":
+#         print("aaaa is", aaaa)
+# else:
+#     print("else", aaaa)
+
+# range
+for i in range(2,10):
+    if(i == 5):
+        continue
+    print("i",i)
+
+if None:
+    print("Hello")
+
