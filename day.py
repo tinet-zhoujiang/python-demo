e86b740e6cfc37dfc7d0067e6aef40dae085b0c8
aoliaodemao <1064272049@qq.com>
2020-05-07 22:28:42 +0800

    20200507

---
+++ day.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+# day = "星期一星期二星期三星期四星期五星期六星期天"
+# n = input("请输入天数(1-7):")
+# m = (int(n)-1)*3
+# dayadd = day[m:m+3]
+# print("中文结果是"+dayadd)
+
+# s = "abcd1234"
+# a = s.find('cd')
+# print (a)
+
+
+# val = pow(2,1000)
+# v = len(str(val))
+# print (v)
+
+for i in range(5):
+    print("*"*(i+1))
+print(5*"1")
+
+    
